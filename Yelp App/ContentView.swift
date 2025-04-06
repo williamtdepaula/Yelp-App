93c4d52a192589f99e34be8efae4f1d31da0156a
william.depaula.agi <william.depaula@agilecontent.com>
2025-04-06 12:13:27 -0300

    Initial Commit

---
+++ Yelp App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yelp App
+//
+//  Created by Willian de Paula on 06/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yelp App/Yelp_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yelp_AppApp.swift
+//  Yelp App
+//
+//  Created by Willian de Paula on 06/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Yelp_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
